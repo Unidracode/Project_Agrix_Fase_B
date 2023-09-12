180ce333f67a4af8fa2dfba877aab7d527772ae3
Unidracode <unidragorn@gmail.com>
2023-09-12 19:20:00 -0300

    requisito 9

--- src/main/java/com/betrybe/agrix/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controller/FertilizerController.java
@@ -3,9 +3,11 @@ package com.betrybe.agrix.controllers;
 import com.betrybe.agrix.dto.FertilizerDto;
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.services.FertilizerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,18 @@ public class FertilizerController {
     FertilizerDto dto = new FertilizerDto(fertilizer.getId(), fertilizer.getName(),
         fertilizer.getBrand(), fertilizer.getComposition());
     return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+  }
+
+  /**
+   * comment.
+   */
+
+  @GetMapping
+  public ResponseEntity<List<FertilizerDto>> findAllFertilizers() {
+    List<Fertilizer> fertilizers = fertilizerService.findAll();
+    List<FertilizerDto> fertilizerDto = fertilizers.stream().map(fertilizer
+        -> new FertilizerDto(fertilizer.getId(), fertilizer.getName(), fertilizer.getBrand(),
+        fertilizer.getComposition())).toList();
+    return ResponseEntity.ok(fertilizerDto);
   }
 }
